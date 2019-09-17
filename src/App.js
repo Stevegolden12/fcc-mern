92406fcda5175acc367527fc1a3541c06339fbca
Steve Golden <sgolden2525@outlook.com>
2019-09-17 08:51:01 -0500

    Added components
    Added components folder
    Added components files
    Added npm package react-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import { BroswerRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component"
 
 
 function App() {
